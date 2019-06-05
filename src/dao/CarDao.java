94a1a9e5080ffdb60720b65f506ac93c8c5ddde8
Memeco <memeco.agra@gmail.com>
2019-06-05 16:21:46 -0300

    Car

--- src/dao/CarDao.java
+++ src/dao/CarDao.java
@@ -2,11 +2,14 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import connectiondb.connectionDb;
 import model.CarBean;
+import model.CarModelBean;
 
 public class CarDao {
 	
@@ -16,7 +19,7 @@ public class CarDao {
 		
 		try {
 			con = connectionDb.getConnection();
-			String sql = "INSERT INTO cars manufacturing_year, chassi, fuel_type, description, price, id_car_brands_fk, id_car_models_fk) VALUES (?, ?, ?, ?, ?, ?, ?);";
+			String sql = "INSERT INTO cars (manufacturing_year, chassi, fuel_type, description, price, quantity, status, id_car_brands_fk, id_car_models_fk) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);";
 			System.out.println(sql);
 			PreparedStatement ps = con.prepareStatement(sql);
 			ps.setInt(1, carBean.getManufacturing_year());
@@ -24,8 +27,10 @@ public class CarDao {
 			ps.setString(3, carBean.getFuel());
 			ps.setString(4, carBean.getDescription());
 			ps.setDouble(5, carBean.getPrice());
-			ps.setInt(6, carBean.getCarBrandBean().getId());
-			ps.setInt(7, carBean.getCarModelBean().getId());
+			ps.setInt(6, carBean.getQuantity());
+			ps.setString(7, carBean.getStatus());
+			ps.setInt(8, carBean.getCarBrandBean().getId());
+			ps.setInt(9, carBean.getCarModelBean().getId());
 
 			ps.executeUpdate();
 
@@ -41,15 +46,123 @@ public class CarDao {
 
 	}
 	
-	public List<CarBean> searchForCarName (String name){
+	public List<CarBean> searchForCarDescription (String description){
+		
+		try {
+			con = connectionDb.getConnection();
+			List<CarBean> newList = new ArrayList<>();
+			String sql = "SELECT a.id_cars, a.manufacturing_year, a.chassi, a.fuel_type, a.description, a.price,\r\n" + 
+					"a.id_car_brands_fk, a.id_car_models_fk, a.quantity, a.status,\r\n" + 
+					"b.id_car_models, b.car_model_name,\r\n" + 
+					"c.id_car_brands, c.car_brand_name \r\n" + 
+					"FROM cars a\r\n" + 
+					"INNER JOIN car_models b ON a.id_car_models_fk = b.id_car_models\r\n" + 
+					"INNER JOIN car_brands c ON a.id_car_brands_fk = c.id_car_brands";
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setString(1,  "%" + description + "%");
+			ResultSet result = ps.executeQuery();
+			
+			while (result.next()) {
+				CarBean carBean = new CarBean();
+				carBean.setId(result.getInt(""));
+				carBean.setChassi(result.getString("chassi"));
+				carBean.setDescription(result.getString("description"));
+				carBean.setFuel(result.getString("fuel_type"));
+				carBean.setManufacturing_year(result.getInt("manufacturing_year"));
+				carBean.setPrice(result.getDouble("price"));
+				carBean.setQuantity(result.getInt("quantity"));
+				carBean.setStatus(result.getString("status"));
+				carBean.getCarBrandBean().setId(result.getInt("id_car_brands"));
+				carBean.getCarBrandBean().setName(result.getString("car_brand_name"));
+				carBean.getCarModelBean().setId(result.getInt("id_car_models"));
+				carBean.getCarModelBean().setName(result.getString("car_model_name"));
+				newList.add(carBean);
+			}
+			return newList;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			connectionDb.closeConnection();
+		}
+		
+		
 		return null;
+		
+		
 	}
 	
 	public List<CarBean> listOfCars(){
+		try {
+			con = connectionDb.getConnection();
+			List<CarBean> newList = new ArrayList<>();
+			String sql = "SELECT a.id_cars, a.manufacturing_year, a.chassi, a.fuel_type, a.description, a.price,\r\n" + 
+					"a.id_car_brands_fk, a.id_car_models_fk, a.quantity, a.status,\r\n" + 
+					"b.id_car_models, b.car_model_name,\r\n" + 
+					"c.id_car_brands, c.car_brand_name \r\n" + 
+					"FROM cars a\r\n" + 
+					"INNER JOIN car_models b ON a.id_car_models_fk = b.id_car_models\r\n" + 
+					"INNER JOIN car_brands c ON a.id_car_brands_fk = c.id_car_brands";
+			PreparedStatement ps = con.prepareStatement(sql);
+			ResultSet result = ps.executeQuery();
+			
+			while (result.next()) {
+				CarBean carBean = new CarBean();
+				carBean.setId(result.getInt(""));
+				carBean.setChassi(result.getString("chassi"));
+				carBean.setDescription(result.getString("description"));
+				carBean.setFuel(result.getString("fuel_type"));
+				carBean.setManufacturing_year(result.getInt("manufacturing_year"));
+				carBean.setPrice(result.getDouble("price"));
+				carBean.setQuantity(result.getInt("quantity"));
+				carBean.setStatus(result.getString("status"));
+				carBean.getCarBrandBean().setId(result.getInt("id_car_brands"));
+				carBean.getCarBrandBean().setName(result.getString("car_brand_name"));
+				carBean.getCarModelBean().setId(result.getInt("id_car_models"));
+				carBean.getCarModelBean().setName(result.getString("car_model_name"));
+				newList.add(carBean);
+			}
+			return newList;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			connectionDb.closeConnection();
+		}
+		
+		
 		return null;
 	}
 	
 	public void EditCar(CarBean carBean) {
+
+		try {
+				con = connectionDb.getConnection();
+				String sql = "UPDATE cars SET manufacturing_year=?, chassi=?, fuel_type=?, description=?, price=?, quantity=?, status=?, id_car_brands_fk=?, id_car_models_fk=? WHERE id_cars=?";
+				System.out.println(sql);
+				PreparedStatement ps = con.prepareStatement(sql);
+
+				ps.setInt(1, carBean.getManufacturing_year());
+				ps.setString(2, carBean.getChassi());
+				ps.setString(3, carBean.getFuel());
+				ps.setString(4, carBean.getDescription());
+				ps.setDouble(5, carBean.getPrice());
+				ps.setInt(6, carBean.getQuantity());
+				ps.setString(7, carBean.getStatus());
+				ps.setInt(8, carBean.getCarBrandBean().getId());
+				ps.setInt(9, carBean.getCarModelBean().getId());;
+				ps.executeUpdate();
+
+
+			} catch (SQLException e) {
+
+				e.printStackTrace();
+
+			} finally {
+
+				connectionDb.closeConnection();
+			}
+
+		
+		
 		
 	}
 	
